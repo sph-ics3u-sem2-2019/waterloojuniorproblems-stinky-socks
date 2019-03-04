0260ff55979345cce8957436200ee99d8d877f93
MrZebarth <kris.zebarth@ocsb.ca>
2019-03-04 09:35:04 -0500

    Initial commit

---
+++ src/ShiftySum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class ShiftySum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		/*
+		 * Follow the instructions in Junior Problem 2.pdf.
+		 * Use the following data to test your program and record your answers
+		 * 1) 1,0
+		 * 2) 1,3
+		 * 3) 99,4
+		 * 4) 10000,5
+		 * 5) 9139,4
+		 * 6) 9252,5
+		 * 7) 888,0
+		 * 8) 4578,1
+		 * 9) 2746,2
+		 * 10) 6295,3
+		 * 11) 6884,4
+		 * 12) 1198,5
+		 * 13) 8655,0
+		 * 14) 2503,1
+		 * 15) 7868,2
+		 */
+	}
+
+}
---
+++ src/Telemarketer.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Telemarketer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Follow the instructions in JuniorProblem.pdf
+		 * Use the following values to test your program and record your results
+		 * 1) 8,6,6,7
+		 * 2) 7,6,6,8
+		 * 3) 9,5,6,9
+		 * 4) 3,4,5,6
+		 * 5) 0,8,8,0
+		 * 6) 8,5,5,9
+		 * 7) 9,1,1,9
+		 * 8) 9,9,9,9
+		 * 9) 9,1,2,8
+		 * 10) 8,7,4,8 
+		 * 11) 8,2,2,7
+		 * 12) 1,3,3,9
+		 * 13) 1,0,2,6
+		 */
+	}
+
+}
